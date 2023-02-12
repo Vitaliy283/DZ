b22250db4b39ddcc448fcfca99ac200cb0e10b24
vitaliy <vgork2805@gmail.com>
2023-02-12 17:31:01 +0700

    задача 41 и 43

---
+++ dz41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+   Console.Write("Введите элемент: ");
+   array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("] ");
+    int count = 0;
+for (int i = 0; i < n; i++)
+{
+    if (array[i] > 0) count++;
+}
+Console.WriteLine("Чисел больше нуля {0}", count);
---
+++ dz43/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 43: Напишите программу, которая0 найдёт точку пересечения двух прямых, заданных уравнениями
+//  y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine("Прямые пересекутся в точке с координатами X: {0}, Y: {1}", x, y);
