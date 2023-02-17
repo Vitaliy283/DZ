4057cf4c05fa196f5cbc8477d6bec169dfd80e4b
vitaliy <vgork2805@gmail.com>
2023-02-17 15:30:25 +0700

    Задачи к семинару 9

---
+++ dz64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу,
+// Которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите натуральное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NToLow(n, count);
+void NToLow(int number, int count)
+{
+    if (n > 0)
+    {
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
+    }
+    else Console.WriteLine("{0} - не натуральное число", n);
+}
---
+++ dz66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+SumElement(m, n);
+Console.WriteLine("Сумма чисел равна {0}", sum);
+
+void SumElement( int m, int n)
+{
+    if (n > m)
+    {
+        for (int i = m; i <= n; i++)
+        {
+            sum += i;
+        }
+    }
+    else
+    {
+        for (int i = n; i <= m; i++)
+        {
+            sum += i;
+        }
+    }
+}
